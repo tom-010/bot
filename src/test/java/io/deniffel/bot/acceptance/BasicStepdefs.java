9eaabdc05f1497682b564b35ccfc58522ac0c6a4
Thomas Deniffel <tdeniffel@googlemail.com>
2018-08-16 22:28:51 +0200

    Cucumber infra

---
+++ src/test/java/io/deniffel/bot/acceptance/BasicStepdefs.java
@@ -0,0 +1,11 @@
+package io.deniffel.bot.acceptance;
+
+import cucumber.api.java.en.When;
+
+public class BasicStepdefs {
+
+    @When("I run a failing step")
+    public void I_run_a_failing_step() throws Throwable {
+
+    }
+}
