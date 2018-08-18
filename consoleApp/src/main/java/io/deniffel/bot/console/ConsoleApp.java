5a08ead23dbc5beff0a6acdfe54e84a1befe12b8
Thomas Deniffel <tdeniffel@googlemail.com>
2018-08-18 14:00:37 +0200

    Console App to chat

--- consoleApp/src/main/java/io/deniffel/bot/console/ConsoleApp.java
+++ consoleApp/src/main/java/io/deniffel/bot/console/ConsoleApp.java
@@ -1,5 +1,26 @@
 package io.deniffel.bot.console;
 
+import java.util.Scanner;
+
 public class ConsoleApp {
-    public static void main(String... args) { }
+    public static void main(String... args) {
+        new ConsoleApp().mainLoop();
+    }
+
+    private void mainLoop() {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        while((input = scanner.nextLine()) != null) {
+            handleInput(input);
+
+            if("exit".equals(input))
+                break;
+        }
+    }
+
+    private void handleInput(String input) {
+        System.out.println(input);
+    }
+
+
 }
