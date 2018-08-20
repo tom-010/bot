11d3527caeb6ad8345173384ef10f2ce7f0b03c4
Thomas Deniffel <tdeniffel@googlemail.com>
2018-08-20 13:01:04 +0200

    Dynamic traversion of plugin files + regex selection

--- loading/Loader.groovy
+++ loading/Loader.groovy
@@ -1,5 +1,54 @@
-new GroovyShell().parse(new File('/home/thomas/Projects/bot/loading/ToLoad.groovy')).with {
-    def (res, ctx) = answer('Thomas');
-    println res
-    println ctx
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
+class PluginManager {
+
+    Map<String, String> scripts = [:];
+
+    private PluginManager() { }
+
+    static PluginManager build() {
+        def manager = new PluginManager()
+        manager.init()
+        return manager
+    }
+
+    def filename() {
+        return new File(getClass().protectionDomain.codeSource.location.path).getName();
+    }
+
+    def init() {
+        Files.walk(Paths.get('.')).forEach{
+            if(it.fileName.toString() == filename() || Files.isDirectory(it))
+                return
+
+            def regex = null;
+            new GroovyShell().parse(it.toFile()).with {
+                 regex = activatorRegex()
+            }
+
+            if(regex != null) {
+                scripts[it.toAbsolutePath().toString()] = regex
+            }
+        }
+    }
+
+    String answer(String message, Map<String, String> ctx = [:]) {
+
+        def result, context
+
+        scripts.forEach { file, activatorRegex ->
+            if(!message.matches(activatorRegex))
+                return;
+
+            new GroovyShell().parse(new File(file)).with {
+                (result, context) = answer(message)
+            }
+        }
+
+        return result
+    }
+
 }
+println PluginManager.build().answer("Hello Tom");
--- loading/ToLoad.groovy
+++ loading/ToLoad.groovy
@@ -1,6 +1,12 @@
 
+def activatorRegex() {
+    return "Hello.*";
+}
+
 def answer(String incomingMessage, Map<String, Object> context = [:]) {
+    
     context.put("Name", "Thomas")
     context.put("Alter", "24")
-    return ["Hello ${incomingMessage}!", context]
+    
+    return ["${incomingMessage}!", context]
 }
